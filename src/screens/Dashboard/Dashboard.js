a3cd8b136471415fd39fb247e6e3c9b5a1aff86b
Qinglei <qinglei@Qingleis-iMac.local>
2021-04-01 23:46:15 +0800

    style - apply css module to dashboard and welcome page

--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -4,64 +4,60 @@ import { Hint } from 'components';
 import { ReactComponent as HealthCircleSVG } from 'resources/icons/HealthCircle.svg';
 import { numberToReadableString } from 'utils/numberUtils';
 import { useWallet } from './WalletContext';
-import './Dashboard.scss';
+
+import styles from './Dashboard.module.scss';
 
 const EXPLAINATION = {
-  healthIndex: "Liquidation will be triggered when the total supply is lower than the total borrowed, health index is calculated by converted supply divide converted borrow",
-  borrowLimit: "The maximum amount you can borrow"
-}
+  healthIndex:
+    'Liquidation will be triggered when the total supply is lower than the total borrowed, health index is calculated by converted supply divide converted borrow',
+  borrowLimit: 'The maximum amount you can borrow',
+};
 
 const COLOR = {
-  blue: "#20EFEF",
-  orange: "#F18B14",
-  pink: "#CF1891"
-}
+  blue: '#20EFEF',
+  orange: '#F18B14',
+  pink: '#CF1891',
+};
 
-const getHealthIndexColor = (healthIndex) => {
+const getHealthIndexColor = healthIndex => {
   if (healthIndex >= 2) {
     return COLOR.blue;
   } else if (healthIndex >= 1) {
     return COLOR.orange;
-  } else if (healthIndex >= 0){
+  } else if (healthIndex >= 0) {
     return COLOR.pink;
   } else {
     return COLOR.blue;
   }
-}
+};
 
-const getHealthIndexText = (healthIndex) => {
+const getHealthIndexText = healthIndex => {
   if (healthIndex >= 10) {
-    return "10.0+";
+    return '10.0+';
   } else if (healthIndex >= 0.1) {
     return healthIndex.toFixed(1);
   } else if (healthIndex === 0) {
-    return "0.0";
+    return '0.0';
   } else if (healthIndex < 0) {
-    return "NA"
-  }{
-    return "<1";
+    return 'NA';
   }
-}
+  {
+    return '<1';
+  }
+};
 
 const HealthCircle = ({ healthIndex }) => {
   if (healthIndex > 2.0 || healthIndex < 0) {
-    return (
-      <HealthCircleSVG stroke={COLOR.blue} />
-    );
+    return <HealthCircleSVG stroke={COLOR.blue} />;
   } else if (healthIndex > 1.0) {
-    return (
-      <HealthCircleSVG stroke={COLOR.orange}/>
-    )
+    return <HealthCircleSVG stroke={COLOR.orange} />;
   }
-  return (
-    <HealthCircleSVG stroke={COLOR.pink} />
-  )
-}
+  return <HealthCircleSVG stroke={COLOR.pink} />;
+};
 
-
-export default function Main (props) {
+export default function Main(props) {
   const { accountBalance } = props;
-  const { prices, setPrices, balances, setBalances} = useWallet();
+  const { prices, setPrices, balances, setBalances } = useWallet();
 
   let result = 0;
   for (let index = 0; index < 5; index++) {
@@ -69,7 +65,11 @@ export default function Main (props) {
   }
 
   const getHealthIndex = () => {
-    if (!accountBalance || accountBalance.debtBalance === 0 || accountBalance.borrowLimit === 0) {
+    if (
+      !accountBalance ||
+      accountBalance.debtBalance === 0 ||
+      accountBalance.borrowLimit === 0
+    ) {
       return -1;
     }
     const index = accountBalance.borrowLimit / accountBalance.debtBalance;
@@ -77,57 +77,60 @@ export default function Main (props) {
       return -1;
     }
     return index;
-  }
+  };
 
   const healthIndex = getHealthIndex();
 
   const getHealthIndexStyle = () => {
-    const color = getHealthIndexColor(healthIndex)
+    const color = getHealthIndexColor(healthIndex);
     if (healthIndex >= 10) {
       return {
         color,
         fontSize: 50,
-        left: 95
+        left: 95,
       };
     }
     return { color };
-  }
+  };
 
   return (
-    <div className="Dashboard-container">
-      <div className="Dashboard-item Dashboard-supply">
-        <p className="Dashboard-cell-label">TOTAL SUPPLY</p>
-        <p className="Dashboard-cell-number">
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <p className={styles.cellLabel}>TOTAL SUPPLY</p>
+        <p className={styles.cellNumber}>
           ${numberToReadableString(accountBalance.supplyBalance, true)}
         </p>
       </div>
-      <div className="Dashboard-item Dashboard-borrow">
-        <p className="Dashboard-cell-label">TOTAL BORROW</p>
-        <p className="Dashboard-cell-number">
+      <div className={styles.item}>
+        <p className={styles.cellLabel}>TOTAL BORROW</p>
+        <p className={styles.cellNumber}>
           ${numberToReadableString(accountBalance.debtBalance, true)}
         </p>
       </div>
-      <div className="Dashboard-item Dashboard-health">
-        <p className="Dashboard-cell-label">HEALTH INDEX <Hint text={EXPLAINATION.healthIndex} /></p>
-        <div className="Dashboard-health-circle">
+      <div className={[styles.item, styles.health].join(' ')}>
+        <p className={styles.cellLabel}>
+          HEALTH INDEX <Hint text={EXPLAINATION.healthIndex} />
+        </p>
+        <div className={styles.circle}>
           <HealthCircle healthIndex={healthIndex} />
         </div>
-        <p className="Dashboard-health-index" style={getHealthIndexStyle()}>
+        <p className={styles.index} style={getHealthIndexStyle()}>
           {getHealthIndexText(healthIndex)}
         </p>
-        <p className="Dashboard-health-threshold">
-          LIQUIDATION THRESHOLD: 1.0
-        </p>
+        <p className={styles.threshold}>LIQUIDATION THRESHOLD: 1.0</p>
       </div>
-      <div className="Dashboard-item Dashboard-borrow-limit">
-        <p className="Dashboard-cell-label">Borrow Limit <Hint text={EXPLAINATION.borrowLimit}/></p>
-        <p className="Dashboard-cell-number">
+      <div className={styles.item}>
+        <p className={styles.cellLabel}>
+          Borrow Limit <Hint text={EXPLAINATION.borrowLimit} />
+        </p>
+
+        <p className={styles.cellNumber}>
           ${numberToReadableString(accountBalance.borrowLimit, true)}
         </p>
       </div>
-      <div className="Dashboard-item Dashboard-apy">
-        <p className="Dashboard-cell-label">TOTAL WALLET BALLANCE</p>
-        <p className="Dashboard-cell-number">
+      <div className={styles.item}>
+        <p className={styles.cellLabel}>TOTAL WALLET BALLANCE</p>
+        <p className={styles.cellNumber}>
           ${numberToReadableString(result, true)}
         </p>
       </div>
--- src/screens/Welcome/WelcomePage.js
+++ src/screens/Welcome/WelcomePage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import AccountButton from 'components/Account/AccountButton';
-import './WelcomePage.scss';
+import styles from  './WelcomePage.module.scss';
 
 export default function Main (props) {
   const { setAccountAddress } = props;
   return (
-    <div className="WelcomePage-container">
-      <p className="WelcomePage-hint">Please connect to your wallet to start</p>
+    <div className={styles.container}>
+      <p className={styles.hint}>Please connect to your wallet to start</p>
       <AccountButton setAccountAddress={setAccountAddress}/>
     </div>
   );
