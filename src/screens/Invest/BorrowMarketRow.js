3bcbee52ac418f942dbf0703746450e2c93f328d
tzhong92 <tzheecspku@gmail.com>
2021-03-31 21:45:57 -0700

    Use real time price to calculate the liquidity

--- src/screens/Invest/BorrowMarketRow.js
+++ src/screens/Invest/BorrowMarketRow.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 import { useSubstrate } from 'services/substrate-lib';
-import { fixed32ToAPY, balanceToUnitNumber, numberToReadableString } from 'utils/numberUtils';
+import {
+  fixed32ToNumber,
+  fixed32ToAPY,
+  balanceToUnitNumber,
+  numberToReadableString,
+} from 'utils/numberUtils';
 import KonomiImage from 'resources/img/KONO.png';
 import DotImage from 'resources/img/DOT.png';
 import KsmImage from 'resources/img/KSM.png';
@@ -11,14 +16,14 @@ import BtcImage from 'resources/img/BTC.png';
 import './BorrowMarketRow.scss';
 
 const BORROW_ASSET_LIST = [
-  { id: 0, name: 'Konomi', abbr: 'KONO', image: KonomiImage, apy: 0.0002, price: 100 },
-  { id: 1, name: 'Polkadot', abbr: 'DOT', image: DotImage, apy: 0.0204, price: 60 },
-  { id: 2, name: 'Kusama', abbr: 'KSM', image: KsmImage, apy: 0.039, price: 5 },
-  { id: 3, name: 'Ethereum', abbr: 'ETH', image: EthImage, apy: 0.0004, price: 600 },
-  { id: 4, name: 'Bitcoin', abbr: 'BTC', image: BtcImage, apy: 0.0078, price: 20000 }
+  { id: 0, name: 'Konomi', abbr: 'KONO', image: KonomiImage },
+  { id: 1, name: 'Polkadot', abbr: 'DOT', image: DotImage },
+  { id: 2, name: 'Kusama', abbr: 'KSM', image: KsmImage },
+  { id: 3, name: 'Ethereum', abbr: 'ETH', image: EthImage },
+  { id: 4, name: 'Bitcoin', abbr: 'BTC', image: BtcImage },
 ];
 
-export default function Main (props) {
+export default function Main(props) {
   const { accountPair, rowId, onClickBorrowMarketRow } = props;
 
   const { api } = useSubstrate();
@@ -26,10 +31,12 @@ export default function Main (props) {
   const [apy, setAPY] = useState(0);
   const [walletBalance, setWalletBalance] = useState('0');
   const [liquidity, setLiquidity] = useState(0);
+  const [price, setPrice] = useState(0);
 
   useEffect(() => {
     let unsubLiquidity = null;
     let unsubWallet = null;
+    let unsubPrice = null;
     const assetId = rowId;
 
     if (assetId != null) {
@@ -37,8 +44,9 @@ export default function Main (props) {
         unsubLiquidity = await api.query.lending.pools(assetId, assetPool => {
           if (assetPool.isSome) {
             const unwrappedPool = assetPool.unwrap();
-            const liquidityInt = balanceToUnitNumber(unwrappedPool.supply)
-                - balanceToUnitNumber(unwrappedPool.debt);
+            const liquidityInt =
+              balanceToUnitNumber(unwrappedPool.supply) -
+              balanceToUnitNumber(unwrappedPool.debt);
             setLiquidity(liquidityInt);
           } else {
             setLiquidity(0);
@@ -47,12 +55,27 @@ export default function Main (props) {
       };
       getLiquidity();
 
+      const getPrice = async () => {
+        unsubPrice = await api.query.assets.price(assetId, price => {
+          if (price) {
+            const newPrice = fixed32ToNumber(price);
+            if (newPrice !== price) {
+              setPrice(newPrice);
+            }
+          }
+        });
+      };
+      getPrice();
+
       if (accountPair) {
         const getBorrowWallet = async () => {
-          unsubWallet = await api.query.assets.balances([assetId, accountPair.address], balance => {
-            const balanceNum = balanceToUnitNumber(balance);
-            setWalletBalance(numberToReadableString(balanceNum));
-          });
+          unsubWallet = await api.query.assets.balances(
+            [assetId, accountPair.address],
+            balance => {
+              const balanceNum = balanceToUnitNumber(balance);
+              setWalletBalance(numberToReadableString(balanceNum));
+            }
+          );
         };
         getBorrowWallet();
       }
@@ -61,11 +84,12 @@ export default function Main (props) {
     return () => {
       unsubLiquidity && unsubLiquidity();
       unsubWallet && unsubWallet();
+      unsubPrice && unsubPrice();
     };
   }, [api.query.assets, api.query.lending, accountPair, rowId]);
 
   useEffect(() => {
-    const interval = setInterval( async () => {
+    const interval = setInterval(async () => {
       const rate = await api.rpc.lending.debtRate(rowId);
       const newAPY = fixed32ToAPY(rate);
       if (newAPY !== apy) {
@@ -77,9 +101,17 @@ export default function Main (props) {
 
   const rowData = BORROW_ASSET_LIST[rowId];
   return (
-    <div className="Market-table-row" key={`borrow ${rowId}`} onClick={() => onClickBorrowMarketRow(rowId)}>
+    <div
+      className="Market-table-row"
+      key={`borrow ${rowId}`}
+      onClick={() => onClickBorrowMarketRow(rowId)}
+    >
       <div className="BorrowMarket-asset-column Market-table-cell">
-        <img className="Market-asset-icon" src={rowData.image} alt="asset-icon" />
+        <img
+          className="Market-asset-icon"
+          src={rowData.image}
+          alt="asset-icon"
+        />
         <p className="Market-table-asset-text">{rowData.abbr}</p>
       </div>
       <div className="BorrowMarket-apy-column">
@@ -89,7 +121,9 @@ export default function Main (props) {
         <p className="Market-table-cell-text">{`${walletBalance} ${rowData.abbr}`}</p>
       </div>
       <div className="BorrowMarket-liquidity-column">
-        <p className="Market-table-cell-text">${numberToReadableString(liquidity * rowData.price, true)}</p>
+        <p className="Market-table-cell-text">
+          ${numberToReadableString(liquidity * price, true)}
+        </p>
       </div>
     </div>
   );
