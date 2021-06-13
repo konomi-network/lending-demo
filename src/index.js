af82163c4ef91f4c3241a54ad1c4795ed273e3be
tzhong92 <tzheecspku@gmail.com>
2021-06-12 18:22:37 -0700

    Set up redux & middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from 'modules/rootReducer';
 
-import App from './App';
+import Main from './App';
 
-render(<App />,
-  document.getElementById('root')
-);
+// pls install devtools to chrome: https://extension.remotedev.io/
+const ENABLE_DEV_TOOLS = process.env.NODE_ENV === 'development';
+
+const store = ENABLE_DEV_TOOLS
+  ? createStore(reducers, composeWithDevTools(applyMiddleware(logger)))
+  : createStore(reducers);
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  );
+}
+
+render(<App />, document.getElementById('root'));
